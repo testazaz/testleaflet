97844a154ad389e32f8d59834043c708c2ecc1f8
testazaz <125652412+testazaz@users.noreply.github.com>
2023-03-30 09:58:00 +0200

    Add files via upload

---
+++ leaflet-search-master/leaflet-search-master/examples/data/Natura2000.js
@@ -0,0 +1,11 @@
+var missionNatura2000 = {
+"type": "FeatureCollection",
+"name": "Natura2000",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nom": "Etude AAA", "Type": "Assistance Technique", "Client": "Client AAA", "Agence": "LMS", "Année_D": "2020", "Année_F": "2020", "Heures": "200", "PDF": "PDF AAA" }, "geometry": { "type": "Point", "coordinates": [ 5.410095442469339, 46.04995816461561 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nom": "Etude BBB", "Type": "Assistance Technique", "Client": "Client BBB", "Agence": "LMS", "Année_D": "2019", "Année_F": "2021", "Heures": "300", "PDF": "PDF BBB" }, "geometry": { "type": "Point", "coordinates": [ 5.673797814481807, 44.829113849743081 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nom": "Etude CCC", "Type": "Assistance Technique", "Client": "Client AAA", "Agence": "Villeurbanne", "Année_D": "2020", "Année_F": "2021", "Heures": "400", "PDF": "PDF CCC" }, "geometry": { "type": "Point", "coordinates": [ 6.503971948595126, 45.444419384438838 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nom": "Etude DDD", "Type": "Assistance Technique", "Client": "Client BBB", "Agence": "Villeurbanne", "Année_D": "2019", "Année_F": "2022", "Heures": "300", "PDF": "PDF DDD" }, "geometry": { "type": "Point", "coordinates": [ 6.455138176000226, 44.125907524376501 ] } }
+]
+}
